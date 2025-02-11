b964674bb32fa45945f86ac83250efff165cadb2
Puru80 <puru.agar99@gmail.com>
2025-02-11 20:57:38 +0530

    BinaryTree

--- Problem1161.java
+++ Problem1161.java
@@ -48,6 +48,41 @@ public class Problem1161 {
         }
     }
 
+    static void dfs(TreeNode node, int level, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+
+        if (level == list.size()) {
+            list.add(node.val);
+        } else {
+            list.set(level, list.get(level) + node.val);
+        }
+
+        dfs(node.left, level + 1, list);
+        dfs(node.right, level + 1, list);
+    }
+
+    //Using DFS
+    public static int maxLevelSumDFS(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        dfs(root, 0, list);
+
+        int max = Integer.MIN_VALUE;
+        int ans = 0;
+
+        pw.println(list.size());
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > max) {
+                max = list.get(i);
+                ans = i + 1;
+            }
+        }
+
+        return ans;
+    }
+
     public static int maxLevelSum(TreeNode root) {
         Queue<TreeNode> q = new LinkedList<>();
 
@@ -56,24 +91,24 @@ public class Problem1161 {
         int ans = 0;
         int level = 0;
         int max = Integer.MIN_VALUE;
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             level += 1;
             int n = q.size();
 
             int sum = 0;
-            for(int i=0;i<n;i++){
+            for (int i = 0; i < n; i++) {
                 TreeNode node = q.poll();
                 sum += node.val;
 
-                if(node.left != null){
+                if (node.left != null) {
                     q.add(node.left);
                 }
-                if(node.right != null){
+                if (node.right != null) {
                     q.add(node.right);
                 }
             }
 
-            if(sum > max){
+            if (sum > max) {
                 ans = level;
                 max = sum;
             }
@@ -92,7 +127,7 @@ public class Problem1161 {
             String[] arr = input.nextLine().split(" ");
 
             TreeNode root = TreeNode.insertLevelOrder(arr);
-            pw.println("Ans: " + maxLevelSum(root));
+            pw.println("Ans: " + maxLevelSumDFS(root));
         }
 
         pw.flush();
